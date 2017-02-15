cad44d60750651133428920639e5e57f8f758c64
Rachel Pappalardo <rachelan@live.unc.edu>
2017-02-15 15:22:26 -0500

    Added links

--- assets/views/index.php
+++ assets/views/index.php
@@ -1,18 +1,18 @@
 
-    <main class="container">
-      <h1>Dylan's LAMP Template</h1>
-      <a href="/signup"><p>Signup</p></a>
-      <a href="/login"><p>Login</p></a>
+<main class="container">
+<center><h1>Dylan's LAMP Template</h1></center>
+<a href="/Signup/index.php"><p>Signup</p></a>
+<a href="/Login/index.php"><p>Login</p></a>
 
-      <a href="/studentquizzes"><p>Student View Quizzes</p></a>
-      <a href="/studentquestion"><p>Student Quiz Question</p></a>
-      <a href="/studentquestion"><p>Student Quiz Results</p></a>
+<a href="/StudentViewQuizzes/index.php"><p>Student View Quizzes</p></a>
+<a href="/StudentQuizQuestion/index.php"><p>Student Quiz Question</p></a>
+<a href="/StudentQuizResults/index.php"><p>Student Quiz Results</p></a>
 
-      <a href="/studentquestion"><p>Prof Quiz List</p></a>
-      <a href="/studentquestion"><p>Prof Create Quiz</p></a>
-      <a href="/studentquestion"><p>Prof Edit Quiz</p></a>
-      <a href="/studentquestion"><p>Prof Gradebook</p></a>
+<a href="/ProfQuizList/index.php"><p>Prof Quiz List</p></a>
+<a href="/ProfCreateQuiz/index.php"><p>Prof Create Quiz</p></a>
+<a href="/ProfEditQuiz/index.php"><p>Prof Edit Quiz</p></a>
+<a href="/ProfGradebook/index.php"><p>Prof Gradebook</p></a>
 
-      <a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
-      <a href="tel:1-408-555-5555">+1 (408) 555-5555</a>
-    </main>
+<a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
+<a href="tel:1-408-555-5555">+1 (408) 555-5555</a>
+</main>
