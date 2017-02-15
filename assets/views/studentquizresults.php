30958851f007c701beef5c0b14fbce3a28795178
Rachel Pappalardo <rachelan@live.unc.edu>
2017-02-15 15:40:40 -0500

    Changed layout

--- assets/views/studentquizresults.php
+++ assets/views/studentquizresults.php
@@ -1,4 +1,5 @@
-<?php
+<center>
+  <?php
 
     $answer1 = $_POST['question-1-answers'];
     $answer2 = $_POST['question-2-answers'];
@@ -14,6 +15,16 @@
     if ($answer4 == "Hypertext Markup Language") { $totalCorrect++; }
     if ($answer5 == "Hello") { $totalCorrect++; }
 
-    echo "<div id='results'>$totalCorrect / 5 correct</div>";
+    echo "<div id='results'>You got $totalCorrect / 5 correct</div>";
+    echo nl2br("\n<div>Cascading Style Sheets (CSS) is a simple mechanism for
+     adding style (e.g., fonts, colors, spacing) to Web documents.
+      These pages contain information on how to learn and use CSS
+      and on available software. They also contain news from the CSS working group</div>");
+
 
 ?>
+<br><object width="425" height="350" data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4" /></object>
+
+</center>
+<br>
+<br>
