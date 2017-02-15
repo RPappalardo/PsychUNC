8f281378a2ea750d1082212b948dfc8bbe1519cf
Rachel Pappalardo <rachelan@live.unc.edu>
2017-02-15 17:48:17 -0500

    Added color to site

--- assets/views/index.php
+++ assets/views/index.php
@@ -1,18 +1,20 @@
 
-<main class="container">
+<main class="container" style= "border-style:solid;
+border-color:#FFFFFF;">
 <center><h1>Dylan's LAMP Template</h1></center>
-<a href="/Signup/index.php"><p>Signup</p></a>
-<a href="/Login/index.php"><p>Login</p></a>
+<a href="/Signup/index.php" ><p id = "bleh">Signup</p></a>
+<a href="/Login/index.php"><p id = "bleh">Login</p></a>
 
-<a href="/StudentViewQuizzes/index.php"><p>Student View Quizzes</p></a>
-<a href="/StudentQuizQuestion/index.php"><p>Student Quiz Question</p></a>
-<a href="/StudentQuizResults/index.php"><p>Student Quiz Results</p></a>
+<a href="/StudentViewQuizzes/index.php"><p id = "bleh">Student View Quizzes</p></a>
+<a href="/StudentQuizQuestion/index.php"><p id = "bleh">Student Quiz Question</p></a>
+<a href="/StudentQuizResults/index.php"><p id = "bleh">Student Quiz Results</p></a>
 
-<a href="/ProfQuizList/index.php"><p>Prof Quiz List</p></a>
-<a href="/ProfCreateQuiz/index.php"><p>Prof Create Quiz</p></a>
-<a href="/ProfEditQuiz/index.php"><p>Prof Edit Quiz</p></a>
-<a href="/ProfGradebook/index.php"><p>Prof Gradebook</p></a>
+<a href="/ProfQuizList/index.php"><p id = "bleh">Prof Quiz List</p></a>
+<a href="/ProfCreateQuiz/index.php"><p id = "bleh">Prof Create Quiz</p></a>
+<a href="/ProfEditQuiz/index.php"><p id = "bleh">Prof Edit Quiz</p></a>
+<a href="/ProfGradebook/index.php"><p id = "bleh">Prof Gradebook</p></a>
 
-<a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
-<a href="tel:1-408-555-5555">+1 (408) 555-5555</a>
+<center><a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
+<a href="tel:1-408-555-5555">+1 (408) 555-5555</a> </center>
+<br>
 </main>
--- assets/views/partials/_footer.php
+++ assets/views/partials/_footer.php
@@ -1,5 +1,6 @@
 
     <!-- Footer -->
+    <br><br>
     <footer class="footer">
       <p>Copyright &copy; <?= date("Y") ?> Rachel Pappalardo/UNC. All Rights Reserved.</p>
     </footer>
