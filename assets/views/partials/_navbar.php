4dc832c2d32d7e097a61022ac47a51008dc1481d
Rachel Pappalardo <rachelan@live.unc.edu>
2017-03-17 01:45:24 -0400

    Changed names

--- assets/views/partials/_navbar.php
+++ assets/views/partials/_navbar.php
@@ -42,11 +42,11 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Settings <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
+                <li><a href="#">User Profile</a></li>
+                <li><a href="#">User Guide</a></li>
+                <li><a href="#">Contact</a></li>
                 <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
+                <li><a href="/logout.php">Logout</a></li>
               </ul>
             </li>
           </ul>
