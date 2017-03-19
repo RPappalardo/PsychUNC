d71398f84f24ec24a41e46789031d81d3f850fed
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2017-03-19 18:01:19 -0400

    fix: signup Register title now Signup

--- assets/views/signup.php
+++ assets/views/signup.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h2>Register</h2>
+                <h2>Sign Up</h2>
                 <hr class="star-light">
             </div>
         </div>
