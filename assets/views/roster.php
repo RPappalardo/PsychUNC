9c3cd0fb077968174a2508c0283675bd75107c6b
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2017-03-19 19:13:17 -0400

    fix: actually added roster page now

---
+++ assets/views/roster.php
@@ -0,0 +1,29 @@
+
+    <main class="container">
+      <h1>Grades</h1>
+
+      <table class="table">
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Average</th>
+        </thead>
+        <tr>
+          <td>Doggo</td>
+          <td>Doggo</td>
+          <!-- TODO: Make hyperlink -->
+          <td>doggo@doggo.dog</td>
+          <td>100%</td>
+        </tr>
+        <tr>
+          <td>Pupper</td>
+          <td>Jr.</td>
+          <td>pupperjr@puppers.pup</td>
+          <td>0%</td>
+        </tr>
+      </table>
+
+      <a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
+      <a href="tel:1-408-555-5555">+1 (408) 555-5555</a>
+    </main>
