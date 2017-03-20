0f9429855c75eac0a27348769103be8d810f8876
Rachel Pappalardo <rachelan@live.unc.edu>
2017-03-19 23:25:55 -0400

    Created simple homepage

--- assets/views/index.php
+++ assets/views/index.php
@@ -1,6 +1,31 @@
 
     <main class="container">
-      <h1>Welcome!</h1>
+      <!-- Jumbotron -->
+           <div class="jumbotron">
+             <h1>Welcome!</h1>
+             <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+             <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+           </div>
+
+           <!-- Example row of columns -->
+           <div class="row">
+             <div class="col-lg-4">
+               <h2>Heading</h2>
+               <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+               <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+             </div>
+             <div class="col-lg-4">
+               <h2>Heading</h2>
+               <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+               <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+             <div class="col-lg-4">
+               <h2>Heading</h2>
+               <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+               <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+             </div>
+           </div>
+
 
       <a href="mailto:someone@yoursite.com?cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">Email Us</a>
       <a href="tel:1-408-555-5555">+1 (408) 555-5555</a>
