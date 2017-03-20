512e4de20048774bec1038a7a5d848a13b4ebfd3
Rachel Pappalardo <rachelan@live.unc.edu>
2017-03-19 23:26:21 -0400

    Made the form's required feauture work again

--- assets/views/signup.php
+++ assets/views/signup.php
@@ -51,7 +51,7 @@
                 <div class="row">
                   <div class="form-group col-xs-12">
                     <button fill="#C0C0C0" type="submit" class="btn btn-default btn-lg">
-                      <a href= "/signup/index.php">Register</a></button>
+                      Register</button>
                     </div>
                 </div>
               </form>
