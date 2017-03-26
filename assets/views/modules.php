43770b6f744de1e93a3204d27c4690cfcd880c77
Rachel Pappalardo <rachelan@live.unc.edu>
2017-03-26 00:15:09 -0400

    Assignment page is not working, must fix

--- assets/views/modules.php
+++ assets/views/modules.php
@@ -1,13 +1,18 @@
     <main class="container">
-      <h1>Modules</h1>
-
+<h1>Chapter 1</h1>
       <div class="list-group">
-        <a href="/modules/assignment" class="list-group-item">Module 1</a>
+        <a href="/modules/assignment/index.php" class="list-group-item">Module 1</a>
         <a href="#" class="list-group-item disabled">Module 2</a>
           <a href="#" class="list-group-item disabled">Module 3</a>
+        </div>
+          <h1>Chapter 2</h1>
+          <div class="list-group">
             <a href="#" class="list-group-item disabled">Module 4</a>
               <a href="#" class="list-group-item disabled">Module 5</a>
                 <a href="#" class="list-group-item disabled">Module 6</a>
+              </div>
+                <h1>Chapter 3</h1>
+<div class="list-group">
                   <a href="#" class="list-group-item disabled">Module 7</a>
                     <a href="#" class="list-group-item disabled">Module 8</a>
       </div>
