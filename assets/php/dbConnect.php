4088f0eec340f38a774e660d6907e3a519bb813d
Rachel Pappalardo <rachelan@live.unc.edu>
2017-03-25 23:06:23 -0400

    Connected database

--- assets/php/dbConnect.php
+++ assets/php/dbConnect.php
@@ -1,2 +1,16 @@
+
 <?php
-  // TODO Connect to database
+$servername = "localhost";
+$username = "rachelan";
+$password = "HSy3ZPonbQCzJ6SA";
+$dbname = "psychunc";
+//Create
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully";
+?>
